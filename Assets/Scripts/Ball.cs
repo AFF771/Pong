0a693ed6ae4c9c65ef16ac90908910ce01878aec
Antonio F <antoniofinisterrafreitas@gmail.com>
2021-03-22 22:52:46 +0000

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    float velocidade = 5f;
+
+    bool lançamento = false;
+    float tempo;
+    float tempoAdicionar;
+
+    [SerializeField]
+    float tempolançamento = 2f;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        tempoAdicionar = Time.deltaTime;
+        tempo += tempoAdicionar;
+        Debug.Log(tempo);
+
+        if (lançamento == false)
+        {
+            if (tempo >= tempolançamento)
+            {
+            GetComponent<Rigidbody2D>().velocity = velocidade * Random.insideUnitSphere;
+            lançamento = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Paddle_1.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle_1 : MonoBehaviour
+{
+    [SerializeField]
+    float velocidade = 0.5f;
+
+    [SerializeField]
+    KeyCode upKey = KeyCode.W;
+
+    [SerializeField]
+    KeyCode downKey = KeyCode.S;
+
+    //double - 16 casas decimais (requer mais computação)
+    //float - 8 casas decimais
+
+   
+    void Update()
+     {
+      
+       if (Input.GetKey(upKey))
+         {
+            //sobe
+            // >> x = x0 + v0 * (x,y,z) * t <<
+
+            transform.position += velocidade * Vector3.up * Time.deltaTime;
+
+            //sem o fator "Time.deltaTime" o movimento depende dos frames por segundo do computador!!
+         }
+       else if (Input.GetKey(downKey))
+         {
+            //desce
+            transform.position += velocidade * Vector3.down * Time.deltaTime;
+         }
+
+
+    float cameraheight = Camera.main.orthographicSize;
+    float halfPaddlesize = 0.5f;
+
+        // Clamp -> (valor, mínimo, máximo)
+
+       Vector3 positionAux = transform.position;
+       positionAux.y = Mathf.Clamp(transform.position.y,
+           -cameraheight + halfPaddlesize, 
+           cameraheight - halfPaddlesize);
+        transform.position = positionAux;
+
+
+     }
+    
+}
